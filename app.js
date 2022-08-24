8e7e16364cda4c7683a317c08fa3057acd0d3bd9
aha1138 <ahadgujar22@gmail.com>
2022-08-24 16:59:28 -0400

    Add changeSize function
    This function allows users to get grid sizes from 2x2 to  64x64

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
+
 function createGrid(size) {
     let grid = document.querySelector(".grid");
+    let units = grid.querySelectorAll("div");
+    units.forEach(div => div.remove());
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
 
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < size * size; i++) {
         let unit = document.createElement("div");
         unit.style.backgroundColor = 'blue';
         grid.insertAdjacentElement("beforeend", unit);
     }
 }
 createGrid(16);
+
+function changeSize(value){
+    createGrid(value);
+}
 
 
 
