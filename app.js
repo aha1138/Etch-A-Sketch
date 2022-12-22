3a4def7f90437d1e841ff29116a44dc8b7346be7
aha1138 <ahadgujar22@gmail.com>
2022-12-22 13:04:50 -0500

    Subject line (try to keep under 50 characters)
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- app.js
+++ app.js
@@ -1,24 +1,60 @@
+var color;
+var slider = document.querySelector('#slider')
+const buttons = document.querySelectorAll('.buttons')
+const grid = document.querySelector('.grid')
+
+
+buttons.forEach(button => button.addEventListener('click', changeColor));
 
 function createGrid(size) {
-    let grid = document.querySelector(".grid");
-    let units = grid.querySelectorAll("div");
-    units.forEach(div => div.remove());
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+    for (let i = 1; i <= size * size; i++) {
+        let gridUnit= document.createElement('div');
+        grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+        grid.insertAdjacentElement('beforeend', gridUnit);
+    } 
+    var gridPixels = grid.querySelectorAll('div');
+    gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', colorGrid));
 
-    for (let i = 0; i < size * size; i++) {
-        let unit = document.createElement("div");
-        unit.style.backgroundColor = 'blue';
-        grid.insertAdjacentElement("beforeend", unit);
-    }
 }
 createGrid(16);
 
+
 function changeSize(value){
-    createGrid(value);
+    createGrid(slider.value);
 }
+function colorGrid(){
+    switch (color){
+        case 'black':
+            this.style.backgroundColor = '#000000';
+            break;
 
+        case 'eraser':
+            this.style.backgroundColor = '#ffffff';
+            break;
 
+                }
 
-  
+            }
+
+// parkercon  rlmoser99
+       
+function changeColor(e) {
+    switch (e.target.dataset.color){
+        case 'black': 
+            color = 'black';
+            break;
+
+        case 'eraser':
+            color = 'white';
+            break; 
+
+        case 'rainbow':
+            color = 'rainbow';
+            break;
+
+        
+    }
+
+}
